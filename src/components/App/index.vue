038ecee2c472e033c8fd8c249bcedcefdd7fcda8
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-20 12:58:28 +0200

    use mailchimp package

--- src/components/App/index.vue
+++ src/components/App/index.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
-    <mailchimp-subscribe
-      login="ueber"
-      data-center="us17"
-      user-id="1bc6d9d915dfd1f9d0950399d"
-      list-id="603ad75049"
-    >
-      <template v-slot="{ subscribe, setEmail, error, success }">
-        <form @submit.prevent="subscribe">
-          <input type="text" @input="setEmail($event.target.value)" />
-          <button type="submit">
-            Submit
-          </button>
-          <div v-if="error">
-            {{ error }}
-          </div>
-          <div v-if="success">
-            YAY!
-          </div>
-        </form>
-      </template>
-    </mailchimp-subscribe>
+  <mailchimp-subscribe
+    login="ueber"
+    data-center="us17"
+    user-id="1bc6d9d915dfd1f9d0950399d"
+    list-id="603ad75049"
+  >
+    <template v-slot="{ subscribe, setEmail, error, success }">
+      <form @submit.prevent="subscribe">
+        <input type="email" @input="setEmail($event.target.value)" />
+        <button type="submit">
+          Submit
+        </button>
+        <div v-if="error">
+          {{ error }}
+        </div>
+        <div v-if="success">
+          YAY!
+        </div>
+      </form>
+    </template>
+  </mailchimp-subscribe>
   </div>
 </template>
 
 <script>
-import MailchimpSubscribe from '@/components/MailchimpSubscribe'
+import MailchimpSubscribe from 'vue-mailchimp-subscribe'
 
 export default {
   components: {
