872d5a0b261a99fc400f698c1b733149730f3906
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-23 09:24:15 +0200

    add hidden input value

--- src/components/NewsletterForm/index.vue
+++ src/components/NewsletterForm/index.vue
@@ -4,6 +4,7 @@
     data-center="us17"
     user-id="1bc6d9d915dfd1f9d0950399d"
     list-id="603ad75049"
+    hidden-input="b_1bc6d9d915dfd1f9d0950399d_603ad75049"
     @success="onSuccess"
   >
     <template v-slot="{ subscribe, setEmail, error, success, loading }">
