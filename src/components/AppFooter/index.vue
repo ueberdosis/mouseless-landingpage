c5671306b19fa3521e9e283a50c0014193f16279
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-23 13:44:00 +0200

    improve footer grid

--- src/components/AppFooter/index.vue
+++ src/components/AppFooter/index.vue
@@ -1,46 +1,54 @@
 <template>
-  <div class="app-footer">
-    <div class="app-footer__navigation small">
-      <ul>
-        <li>
-          <a href="#features">
-            Features
-          </a>
-        </li>
-        <li>
-          <a href="#download">
-            Download
-          </a>
-        </li>
-        <li>
-          <a href="#buy">
-            Buy
-          </a>
-        </li>
-        <li>
-          <a href="#impressum">
-            Impressum
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="app-footer__newsletter small">
-      <h3>
-        Be the first to know
-      </h3>
-      <p>
-        We'll inform you about new apps and deals. No spam, we promise.
-      </p>
-      <newsletter-form />
-    </div>
+  <div class="app-footer small">
+    <grid :options="{ default: 'backwards wide', medium: 'forwards' }">
+      <grid-item :options="{ medium: '6/12' }">
+        <ul>
+          <li>
+            <a href="#features">
+              Features
+            </a>
+          </li>
+          <li>
+            <a href="#download">
+              Download
+            </a>
+          </li>
+          <li>
+            <a href="#buy">
+              Buy
+            </a>
+          </li>
+          <li>
+            <a href="#impressum">
+              Impressum
+            </a>
+          </li>
+        </ul>
+        <p>
+          © 2019 überclub
+        </p>
+      </grid-item>
+      <grid-item :options="{ medium: '6/12' }">
+        <h3>
+          Be the first to know
+        </h3>
+        <p>
+          We'll inform you about new apps and deals. No spam, we promise.
+        </p>
+        <newsletter-form />
+      </grid-item>
+    </grid>
   </div>
 </template>
 
 <script>
+import { Grid, GridItem } from '@/components/Grid'
 import NewsletterForm from '@/components/NewsletterForm'
 
 export default {
   components: {
+    Grid,
+    GridItem,
     NewsletterForm,
   },
 }
