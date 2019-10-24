a1163e66542f6f1c605c6ee5bdba3101ee1bfb57
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-24 14:27:01 +0200

    add console warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
   'extends': [
     'scrumpy',
   ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
   parserOptions: {
     parser: 'babel-eslint',
   },
