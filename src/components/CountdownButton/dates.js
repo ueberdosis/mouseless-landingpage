4b0ce738c5ca019070cb6afa9dd8756b20b19a9a
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-24 15:37:18 +0200

    add discount list

---
+++ src/components/CountdownButton/dates.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    date: '25.10.2019',
+    title: 'Launch Day',
+  },
+  {
+    date: '31.10.2019',
+    title: 'Halloween',
+  },
+  {
+    date: '03.11.2019',
+    title: 'Zero Tasking Day',
+  },
+  {
+    date: '4.11.2019',
+    title: 'Use Your Common Sense Day',
+  },
+  {
+    date: '10.11.2019',
+    title: 'World Science Day for Peace and Development',
+  },
+  {
+    date: '12.11.2019',
+    title: 'Happy Hour Day',
+  },
+  {
+    date: '20.11.2019',
+    title: 'Name Your PC Day',
+  },
+  {
+    date: '26.11.2019',
+    title: 'Cake Day',
+  },
+  {
+    date: '29.11.2019',
+    title: 'Black Friday',
+  },
+  {
+    date: '01.12.2019',
+    title: '1. Advent',
+  },
+  {
+    date: '04.12.2019',
+    title: 'Cookie Day',
+  },
+  {
+    date: '05.12.2019',
+    title: 'International Ninja Day',
+  },
+  {
+    date: '08.12.2019',
+    title: '2. Advent',
+  },
+  {
+    date: '15.12.2019',
+    title: '3. Advent',
+  },
+  {
+    date: '22.12.2019',
+    title: '4. Advent',
+  },
+  {
+    date: '26.12.2019',
+    title: 'Thank You Note Day',
+  },
+  {
+    date: '29.12.2019',
+    title: 'Tick Tock Day',
+  },
+]
--- src/components/CountdownButton/index.vue
+++ src/components/CountdownButton/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="countdown-button">
+  <div class="countdown-button" :class="{ 'has-discount': activeDiscount }">
     <div class="countdown-button__inner">
       <countdown
         class="countdown-button__countdown"
-        :time="date"
+        :time="activeDiscount.countdown"
         :interval="1000"
         tag="div"
+        v-if="activeDiscount"
       >
         <template v-slot="{ days, hours, minutes, seconds }">
           <div class="countdown-button__item">
@@ -43,17 +44,26 @@
         </template>
       </countdown>
       <btn :href="gumroadLink" target="_blank" class="countdown-button__button">
-        Buy for <s>$30</s> <span class="green">$15</span>
+        Buy for
+        <template v-if="activeDiscount">
+          <s>$30</s> <span class="green">$15</span>
+        </template>
+        <template v-else>
+          $30
+        </template>
       </btn>
     </div>
-    <div class="countdown-button__discount">
-      50% off on Launch Day
+    <div class="countdown-button__discount" v-if="activeDiscount">
+      50% off on {{ activeDiscount.title }}
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+import collect from 'collect.js'
 import Countdown from '@chenfengyuan/vue-countdown'
+import dates from './dates'
 import Btn from '@/components/Btn'
 
 export default {
@@ -69,12 +79,37 @@ export default {
   },
 
   data() {
-    const now = new Date()
-    const newYear = new Date(now.getFullYear() + 1, 0, 1)
+    const now = moment.utc()
+    const dateParseFormat = 'DD.MM.YYYY hh:mm:ss'
 
     return {
-      date: newYear - now,
+      date: now.format('DD.MM.YY hh:mm:ss'),
+      formattedDates: collect(dates)
+        .map(item => {
+          const end = moment(item.date, dateParseFormat).utc()
+          const start = moment(end).subtract(1, 'day')
+          const isActive = moment(now).isBetween(start, end)
+          const countdown = isActive
+            ? moment.duration(end.diff(now)).asMilliseconds()
+            : 0
+
+          return {
+            ...item,
+            start: start.format('YYYY-MM-DD hh:mm:ss'),
+            end: end.format('YYYY-MM-DD hh:mm:ss'),
+            isActive,
+            countdown,
+          }
+        })
+        .sortBy('end')
+        .toArray(),
     }
+  },
+
+  computed: {
+    activeDiscount() {
+      return this.formattedDates.find(item => item.isActive)
+    },
   },
 }
 </script>
