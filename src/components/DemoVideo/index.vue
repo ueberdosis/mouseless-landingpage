021aeeb0ab630b7c606332b261fafd6a07229669
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-25 10:42:21 +0200

    fix video size

--- src/components/DemoVideo/index.vue
+++ src/components/DemoVideo/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <video
-    class="demo-video"
-    src="@/assets/videos/test.mp4"
-    playsinline
-    autoplay
-    muted
-    loop
-  />
+  <div class="demo-video">
+    <div class="demo-video__ratio">
+      <video
+        class="demo-video__video"
+        src="@/assets/videos/test.mp4"
+        playsinline
+        autoplay
+        muted
+        loop
+      />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" src="./style.scss"></style>
