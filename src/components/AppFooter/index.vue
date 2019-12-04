4f634009edf5044462c6751321901a67128cc1c5
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-04 11:21:46 +0100

    improve footer style

--- src/components/AppFooter/index.vue
+++ src/components/AppFooter/index.vue
@@ -1,58 +1,60 @@
 <template>
   <div class="app-footer small">
-    <div>
-      <div v-for="app in apps" :key="app.id">
+    <div class="app-footer__section app-footer__apps">
+      <div class="app-footer__app" v-for="app in apps" :key="app.id">
         <g-link :to="`/app/${app.id}`">
-          {{ app.title }}
+          {{ app.title }} Shortcuts
         </g-link>
       </div>
     </div>
-    <grid :options="{ default: 'backwards wide', medium: 'forwards' }">
-      <grid-item :options="{ medium: '6/12' }">
-        <ul>
-          <li>
-            <a href="#features">
-              Features
-            </a>
-          </li>
-          <li>
-            <a :href="downloadLink" download>
-              Download
-            </a>
-          </li>
-          <li>
-            <a :href="gumroadLink" target="_blank">
-              Buy
-            </a>
-          </li>
-          <li>
-            <a href="https://floatie.app/mouseless" target="_blank">
-              Open
-            </a>
-          </li>
-          <li>
-            <a href="https://ueberdosis.io/impressum" target="_blank">
-              Impressum
-            </a>
-          </li>
-        </ul>
-        <p>
-          Made with 💛 by <a href="https://twitter.com/_ueberdosis" target="_blank">überclub</a>
-          · Icon designed by <a href="https://twitter.com/juliusgehrig" target="_blank">Julius Gehrig</a>
-        </p>
-      </grid-item>
-      <grid-item :options="{ medium: '6/12' }">
-        <h3>
-          Be the first to know
-        </h3>
-        <p>
-          We’ll inform you about new apps and deals.
-          <br>
-          No spam, we promise.
-        </p>
-        <newsletter-form />
-      </grid-item>
-    </grid>
+    <div class="app-footer__section">
+      <grid :options="{ default: 'backwards wide', medium: 'forwards' }">
+        <grid-item :options="{ medium: '6/12' }">
+          <ul>
+            <li>
+              <a href="#features">
+                Features
+              </a>
+            </li>
+            <li>
+              <a :href="downloadLink" download>
+                Download
+              </a>
+            </li>
+            <li>
+              <a :href="gumroadLink" target="_blank">
+                Buy
+              </a>
+            </li>
+            <li>
+              <a href="https://floatie.app/mouseless" target="_blank">
+                Open
+              </a>
+            </li>
+            <li>
+              <a href="https://ueberdosis.io/impressum" target="_blank">
+                Impressum
+              </a>
+            </li>
+          </ul>
+          <p>
+            Made with 💛 by <a href="https://twitter.com/_ueberdosis" target="_blank">überclub</a>
+            · Icon designed by <a href="https://twitter.com/juliusgehrig" target="_blank">Julius Gehrig</a>
+          </p>
+        </grid-item>
+        <grid-item :options="{ medium: '6/12' }">
+          <h3>
+            Be the first to know
+          </h3>
+          <p>
+            We’ll inform you about new apps and deals.
+            <br>
+            No spam, we promise.
+          </p>
+          <newsletter-form />
+        </grid-item>
+      </grid>
+    </div>
   </div>
 </template>
 
