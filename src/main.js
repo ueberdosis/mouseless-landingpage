3db9615030d1d6e0d75b1d94b17a6c78116a2b32
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-06 11:35:52 +0100

    update version

--- src/main.js
+++ src/main.js
@@ -61,7 +61,7 @@ export default function (Vue, { router, head, isClient }) {
     data() {
       return {
         activeDiscount: formattedDiscountDates.find(item => item.isActive),
-        downloadLink: 'https://ueber.fra1.cdn.digitaloceanspaces.com/mouseless/mac/Mouseless-1.1.0-mac.dmg',
+        downloadLink: 'https://ueber.fra1.cdn.digitaloceanspaces.com/mouseless/mac/Mouseless-1.1.1-mac.dmg',
       }
     },
 
