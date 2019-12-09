3008171b19592eda30af6ad77ac93db747b51d3f
Hans Pagel <hans.pagel@ueber.io>
2019-12-09 14:24:53 +0100

    add more discounts

--- src/discountDates.js
+++ src/discountDates.js
@@ -48,19 +48,135 @@ export default [
     title: '2nd Advent',
   },
   {
+    date: '2019-12-09',
+    title: 'National Pastry Day',
+  },
+  {
+    date: '2019-12-10',
+    title: 'Nobel Prize Day',
+  },
+  {
+    date: '2019-12-11',
+    title: 'National App Day',
+  },
+  {
+    date: '2019-12-12',
+    title: 'Gingerbread House Day',
+  },
+  {
+    date: '2019-12-13',
+    title: 'Friday the 13th',
+  },
+  {
+    date: '2019-12-14',
+    title: 'Monkey Day',
+  },
+  {
     date: '2019-12-15',
     title: '3rd Advent',
+  },
+  {
+    date: '2019-12-16',
+    title: 'National Chocolate-covered Anything Day',
+  },
+  {
+    date: '2019-12-17',
+    title: 'Wright Brothers Day',
+  },
+  {
+    date: '2019-12-18',
+    title: 'Arabic Language Day',
+  },
+  {
+    date: '2019-12-19',
+    title: 'National Hard Candy Day',
+  },
+  {
+    date: '2019-12-20',
+    title: 'National Re-Gifting Day',
+  },
+  {
+    date: '2019-12-21',
+    title: 'Underdog Day',
   },
   {
     date: '2019-12-22',
     title: '4th Advent',
   },
   {
+    date: '2019-12-23',
+    title: 'Hanukkah',
+  },
+  {
+    date: '2019-12-24',
+    title: 'Christmas Eve',
+  },
+  {
+    date: '2019-12-25',
+    title: 'Pumpkin Pie Day',
+  },
+  {
     date: '2019-12-26',
     title: 'Thank You Note Day',
   },
   {
+    date: '2019-12-27',
+    title: 'National Fruitcake Day',
+  },
+  {
+    date: '2019-12-28',
+    title: 'National Short Film Day',
+  },
+  {
     date: '2019-12-29',
     title: 'Tick Tock Day',
+  },
+  {
+    date: '2019-12-30',
+    title: 'National Bacon Day',
+  },
+  {
+    date: '2019-12-31',
+    title: 'Last Day of 2019',
+  },
+  {
+    date: '2020-01-01',
+    title: 'New Year',
+  },
+  {
+    date: '2020-01-02',
+    title: 'Science Fiction Day',
+  },
+  {
+    date: '2020-01-03',
+    title: 'Chocolate Covered Cherry Day',
+  },
+  {
+    date: '2020-01-04',
+    title: 'National Spaghetti Day',
+  },
+  {
+    date: '2020-01-05',
+    title: 'National Bird Day',
+  },
+  {
+    date: '2020-01-06',
+    title: 'National Technology Day',
+  },
+  {
+    date: '2020-01-07',
+    title: 'International Programmers\' Day',
+  },
+  {
+    date: '2020-01-08',
+    title: 'National English Toffee Day',
+  },
+  {
+    date: '2020-01-09',
+    title: 'National Apricot Day',
+  },
+  {
+    date: '2020-01-10',
+    title: 'National Bittersweet Chocolate Day',
   },
 ]
