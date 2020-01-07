d469b692d10700f39e1c1eec2e6d9934cce1518c
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-07 14:48:38 +0100

    add support link to footer

--- src/components/AppFooter/index.vue
+++ src/components/AppFooter/index.vue
@@ -10,33 +10,46 @@
     <div class="app-footer__section">
       <grid :options="{ default: 'backwards wide', medium: 'forwards' }">
         <grid-item :options="{ medium: '6/12' }">
-          <ul>
-            <li>
-              <g-link to="/">
-                Features
-              </g-link>
-            </li>
-            <li>
-              <a :href="downloadLink" download>
-                Download
-              </a>
-            </li>
-            <li>
-              <a :href="gumroadLink" target="_blank">
-                Buy
-              </a>
-            </li>
-            <li>
-              <a href="https://floatie.app/mouseless" target="_blank">
-                Public Metrics
-              </a>
-            </li>
-            <li>
-              <a href="https://ueberdosis.io/impressum" target="_blank">
-                Impressum
-              </a>
-            </li>
-          </ul>
+          <grid :options="{ default: 'narrow', medium: 'wide' }">
+            <grid-item :options="{ medium: '6/12' }">
+              <ul>
+                <li>
+                  <g-link to="/">
+                    Features
+                  </g-link>
+                </li>
+                <li>
+                  <a :href="downloadLink" download>
+                    Download
+                  </a>
+                </li>
+                <li>
+                  <a :href="gumroadLink" target="_blank">
+                    Buy
+                  </a>
+                </li>
+              </ul>
+            </grid-item>
+            <grid-item :options="{ medium: '6/12' }">
+              <ul>
+                <li>
+                  <a href="mailto:support@mouseless.app" target="_blank">
+                    Support
+                  </a>
+                </li>
+                <li>
+                  <a href="https://floatie.app/mouseless" target="_blank">
+                    Public Metrics
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ueberdosis.io/impressum" target="_blank">
+                    Impressum
+                  </a>
+                </li>
+              </ul>
+            </grid-item>
+          </grid>
           <p>
             Made with 💛 by <a href="https://twitter.com/_ueberdosis" target="_blank">überclub</a>
             · Icon designed by <a href="https://twitter.com/juliusgehrig" target="_blank">Julius Gehrig</a>
